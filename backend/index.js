3b436680d5598a88b96fe90f394a3f67183f820c
diegobermudez09 <diegobl9do@gmail.com>
2024-07-19 17:46:35 -0500

    cambios

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,24 @@
 import express from "express";
 import dotenv from 'dotenv';
 import conectarDB from "./config/db.js";
-import veterinarioRoutes from "./routes/veterinarioRoutes.js"
-import pacienteRoutes from "./routes/pacienteRoutes.js"
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacienteRoutes from "./routes/pacienteRoutes.js";
+
+// Cargar variables de entorno
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-dotenv.config();
-
+// Conectar a la base de datos
 conectarDB();
 
-
+// Rutas
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
 
-const PORT = process.env.PORT || 4000
-
+// Obtener el puerto desde las variables de entorno o usar 5173 por defecto
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`funcionando en el puerto ${PORT}`)
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
