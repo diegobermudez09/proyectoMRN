2304ae0bec8ac0ce13f6234226a4596cb023fcde
diegobermudez09 <diegobl9do@gmail.com>
2024-07-19 16:25:27 -0500

    terminado el CRUD del backend

---
+++ backend/routes/pacienteRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const router = express.Router();
+import { agregarPaciente, obtenerPacientes,obtenerPaciente, actualizarPaciente, eliminarPaciente } from '../controllers/pacienteController.js';
+import checkAuth from '../middleware/authMiddleware.js';
+
+router
+    .route('/')
+    .post(checkAuth, agregarPaciente)
+    .get(checkAuth, obtenerPacientes);
+
+router
+    .route("/:id")
+    .post(checkAuth, obtenerPaciente)
+    .put(checkAuth, actualizarPaciente)
+    .delete(checkAuth, eliminarPaciente)
+
+
+
+export default router;
